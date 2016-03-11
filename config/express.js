83527f23ab1bbf49e791e2b361c39d91c0a24408
Caio Cutrim <caiocutrim@users.noreply.github.com>
2016-03-10 21:50:14 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -1,15 +1,15 @@
 /**
-<<<<<<< HEAD
-configurations of the middleware
+*
+* configurations of the middleware
 **/
 "use strict";
 module.exports = Setup;
-=======
-  configurations of the middleware
- **/
+
+// configurations of the middleware
+
 'use strict';
 module.exports =Setup;
->>>>>>> 872111e8a6e3525cfa5f27f121947a069f35bfd9
+
 const express = require('express');
 const passport = require('passport');
 const methodOverride = require('method-override');
@@ -19,35 +19,35 @@ const engine = require('ejs-mate');
 let app;
 
 
-<<<<<<< HEAD
+
 function Setup() {
   app = express();
   app.engine('ejs', engine);
-=======
+
 function Setup(){
   app = express();
   app.engine('ejs',engine);
->>>>>>> 872111e8a6e3525cfa5f27f121947a069f35bfd9
+
   app.set("views", "./app/views"); //especifica o modulo onde serão criadas  as views
   app.set("view engine", "ejs"); // especifica o motor de viwes
   app.set('port', 3000);
 
   //usando middlewares incluidos no express
   app.use(require('express-session')({
-<<<<<<< HEAD
+
     secret: 'my secret',
     resave: false,
     saveUninitilized: false
-=======
+
     secret:'my secret',
     resave:false,
     saveUninitialized:false
->>>>>>> 872111e8a6e3525cfa5f27f121947a069f35bfd9
+
   }));
   app.use(passport.initialize());
   app.use(express.static('public'));
   app.use(bodyParser.json());
-<<<<<<< HEAD
+
   app.use(bodyParser.urlencoded({
     extended: true
   }));
@@ -56,12 +56,12 @@ function Setup(){
   load('model', {
       cwd: 'app'
     })
-=======
+
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(methodOverride());
 
   load('model',{cwd:'app'})
->>>>>>> 872111e8a6e3525cfa5f27f121947a069f35bfd9
+
     .then('controllers')
     .then('routes')
     .into(app);
